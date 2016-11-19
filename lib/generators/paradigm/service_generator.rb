f2041ef83b1c4d230435f482bb6640104660ef40
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2016-11-19 11:57:42 -0500

    Add nested generators functionality

--- lib/generators/paradigm/service_generator.rb
+++ lib/generators/paradigm/service_generator.rb
@@ -6,8 +6,25 @@ module Paradigm
       source_root File.expand_path('../../templates/service', __FILE__)
 
       def create_service_file
-        template 'service.rb',      "app/services/#{file_name.underscore}_service.rb"
-        template 'service_test.rb', "test/services/#{file_name.underscore}_service_test.rb"
+        @file_name = file_name
+
+        if nested_file_name?
+          @path = file_name.partition(':').delete_if { |x| x == ':' }
+
+          template 'nested_service.rb', "app/services/#{path[0]}/#{path[1].underscore}_service.rb"
+          template 'nested_service_test.rb', "test/services/#{path[0]}/#{path[1].underscore}_service_test.rb"
+        else
+          template 'service.rb', "app/services/#{file_name.underscore}_service.rb"
+          template 'service_test.rb', "test/services/#{file_name.underscore}_service_test.rb"
+        end
+      end
+
+      private
+
+      attr_reader :file_name, :path
+
+      def nested_file_name?
+        file_name.include?(':')
       end
     end
   end
---
+++ lib/generators/templates/service/nested_service_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class <%= @path[0].camelcase %>::<%= @path[1].camelcase %>ServiceTest < ActiveSupport::TestCase
+  # test '.call' do
+  #   assert true
+  # end
+end
